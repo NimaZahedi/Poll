8cc382f77eb9110274a78dd5d7df795023738992
NimaZahedi <Nima.Zahedee@gmail.com>
2015-11-18 20:07:18 +0100

    add winner component

--- app/services/project_services.ts
+++ app/services/project_services.ts
@@ -1,17 +1,22 @@
 export class ProjectServices {
 	private projects: Firebase;
-	private url = "https://AngularJSCPH.firebaseio.com/Projects";
-	
+	private url = "https://AngularJSCPH.firebaseio.com/Projects/";
+
 	constructor() {
-		this.projects = new Firebase(this.url); 
+		this.projects = new Firebase(this.url);
 	}
-	
+
 	public push(project) {
 		this.projects.push(project);
 	}
-	
+
+	public getProjectById(id, callback) {
+		var projectRef = new Firebase(this.url + id);
+		projectRef.once('value', callback);
+	}
+
 	public on(callback) {
 		this.projects.on('value', callback);
 	}
-	
+
 }
