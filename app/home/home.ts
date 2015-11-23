1afe3ef61596c0f705774485689c5ef3fdd8c796
jahtalab <h.jahtalab@gmail.com>
2015-11-23 12:48:37 +0100

    added some styling

--- app/home/home.ts
+++ app/home/home.ts
@@ -1,16 +1,20 @@
-import {Component} from 'angular2/angular2';
-import {Router} from 'angular2/router';
+import {Component, NgIf} from 'angular2/angular2';
+import {Router, RouterLink} from 'angular2/router';
 import {AuthServices} from "../services/auth_services";
 
 @Component({
 	selector: 'home',
-	templateUrl: '../app/home/home.html'
+	templateUrl: '../app/home/home.html',
+	directives: [NgIf, RouterLink]
+
 })
 
 export class Home {
 	constructor(private authServices: AuthServices, private router: Router) {
 	}
-
+	public isAuthenticate() {
+		return this.authServices.isAuthenticated();
+	}
 	public initGoogleAuth() {
 		this.authServices.initGoogleAuth(() => {
 			this.router.navigateByUrl('/project');
